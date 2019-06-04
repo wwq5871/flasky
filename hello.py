6a905c8f87f9fd748d5bef9e6c797a283745d733
mikael wang <a819453309@163.com>
2019-06-04 09:41:15 +0800

    local dev

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
+from flask import request
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User_Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello,{}!</h1>'.format(name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
